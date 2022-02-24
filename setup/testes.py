060991ce8bd5d4b7f498d9ea2095e0a3d34a3b34
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2022-02-24 19:24:11 -0400

    Adicionando app de animais

--- setup/testes.py
+++ setup/testes.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class AnimaisTestCase(LiveServerTestCase):
     ## etapas para ser executadas antes de iniciar os testes
@@ -11,10 +12,16 @@ class AnimaisTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit() ## fecha o browser
 
-    def test_abre_janela_do_chrome(self):
-        ''' Teste para abrir a janela do servidor '''
-        self.browser.get(self.live_server_url) #vai para o endereço
+    def test_buscando_um_novo_animal(self):
+        '''Teste se um usuario encontra um animal pesquisando'''
+        home_page = self.browser.get(self.live_server_url + '/') #vai para o endereço
+        brand_element = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
+        self.assertEqual('Busca Animal', brand_element.text)
+    
+    # def test_abre_janela_do_chrome(self):
+    #     ''' Teste para abrir a janela do servidor '''
+    #     self.browser.get(self.live_server_url) #vai para o endereço
 
-    def test_deu_ruim(self):
-        '''Teste de exemplo que irá falhar'''
-        self.fail("deu ruim")
+    # def test_deu_ruim(self):
+    #     '''Teste de exemplo que irá falhar'''
+    #     self.fail("deu ruim")
