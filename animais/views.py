44cf0a6f127ef802c6affd141ac142b0d980ec41
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2022-02-24 20:39:22 -0400

    Finalizando projeto

--- animais/views.py
+++ animais/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from animais.models import Animal
 
-def index(requisicao):
-    context = {'caracteristicas': None}
-    if 'buscar' in requisicao.GET:
-        animais = Animal.objects.all()
-        animal_pesquisado = requisicao.GET['buscar']
+def index(request):
+    animais = Animal.objects.all()
+    context = {'caracteristicas' : animais}
+
+    if 'buscar' in request.GET:
+        animal_pesquisado = request.GET['buscar']
         caracteristicas = animais.filter(nome_animal__icontains = animal_pesquisado)
         context = {'caracteristicas': caracteristicas}
-    return render(requisicao, 'index.html', context)
+    return render(request, 'index.html', context)
